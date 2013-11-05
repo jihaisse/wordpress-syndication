aa17d3f512b5271639e3292a72510bd44001eba2
Jihaisse <jean-sebastien.mansart@laposte.net>
2013-11-05 14:24:22 +0100

    first working version

--- rel-syndication.php
+++ rel-syndication.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/jihaisse/wordpress-syndication
 Description: 
 Author: Jean-Sébastien Mansart
 Author URI: http://jihais.se
-Version: 0.1.2
+Version: 0.1
 License: GPL2++
 */
 
@@ -19,17 +19,66 @@ function rel_syndication_uninstall() {
 
 add_filter( 'the_content', 'add_js_rel_syndication', 20);
 function add_js_rel_syndication($content) {
-	$content = $content."
-	<ul>
-		<li>
-			<a class=\"u-syndication\" rel=\"syndication\" href=\"https://twitter.com/aaronpk/status/391335890179469312\">View on Twitter</a>
-		</li>
-	</ul>";
+	$see_on = "";
 	
-	$meta_values = get_post_meta( get_the_ID(), "_social_broadcasted_ids", true );
+	if (class_exists("Social")){
+		$Social = new Social();
+		$twitter = new Social_Service_Twitter();
+		$Social_View = new Social_View();
+		
+		$ids = get_post_meta( get_the_ID(), "_social_broadcasted_ids", true );
 	
-	$content =  $content.print_r($meta_values['twitter']);
-	
+		$services = $Social->instance()->services();
+		$broadcasts = array();
+		
+		if (is_array($ids) and count($ids)) {
+			foreach ($services as $key => $service) {
+				if (isset($ids[$key]) and count($ids[$key])) {
+					$broadcasted = true;
+					foreach ($ids[$key] as $user_id => $broadcasted) {
+						$account = $service->account($user_id);
+						if (empty($output)) {
+							$accounts_output = '<ul class="social-broadcasted">';
+						}
+
+						foreach ($broadcasted as $broadcasted_id => $data) {
+							if ($account === false) {
+								$class = 'Social_Service_'.$key.'_Account';
+								$account = new $class($data['account']);
+
+								if (!$account->has_user() and $key == 'twitter') {
+									$recovered = $service->recover_broadcasted_tweet_data($broadcasted_id, $post->ID);
+
+									if (isset($recovered->user)) {
+										$data['account']->user = $recovered->user;
+										$account = new $class($data['account']);
+									}
+								}
+							}
+
+							$broadcasted = esc_html($service->title());
+							if (isset($broadcasted_id)) {
+								if ($account->has_user() or $service->key() != 'twitter') {
+									$url = $service->status_url($account->username(), $broadcasted_id);
+									if (!empty($url)) {
+										$broadcasted = '<a class="u-syndication" rel="syndication" href="'.esc_url($url).'" target="_blank">'.esc_html($service->title()).'</a>';
+									}
+								}
+							}
+							array_push($broadcasts,"<li>".$broadcasted."</li>");
+						}
+					}
+
+					if (count($broadcasts)) {
+						$see_on = '<ul>'.implode("\n", $broadcasts).'</ul>';
+					}
+				}
+			}
+		}
+	}
+	if ($see_on !== ""){
+		$content = $content . '<div class="usyndication">Also on : '.$see_on."</div>";
+	}
 	// Returns the content.
    	return $content;
 }
