5dffea6073f20f82f4f290fccab76c5ea0e7ddb8
Peter Molnar <hello@petermolnar.eu>
2014-05-16 09:59:22 +0100

    SNAP twitter, tumblr, facebook support, alpha release

--- rel-syndication.php
+++ rel-syndication.php
@@ -3,11 +3,12 @@
 Plugin Name: rel-syndication for wordpress
 Plugin URI: https://github.com/jihaisse/wordpress-syndication
 Text Domain: rel-syndication
-Description: 
+Description:
 Author: Jean-Sébastien Mansart
 Author URI: http://jihais.se
-Version: 0.1
+Version: 0.2
 License: GPL2++
+Contributors: Peter Molnar
 */
 
 // function to add markup at the end of the post
@@ -15,15 +16,19 @@ add_filter( 'the_content', 'add_js_rel_syndication', 20);
 function add_js_rel_syndication($content) {
 	load_plugin_textdomain( 'rel-syndication', false, dirname( plugin_basename(__FILE__) ) . '/languages/' );
 	$see_on = "";
-	
+
 	if (class_exists("Social")){
 		$see_on = getRelSyndicationFromSocial();
 	}
+	elseif ( defined ( 'NextScripts_SNAP_Version' ) ) {
+		$see_on = getRelSyndicationFromSNAP();
+	}
+
 	if ($see_on !== ""){
 		$content = $content . '<div class="usyndication">' . __("Also on:", "rel-syndication") . $see_on . "</div>";
 	}
 	// Returns the content.
-   	return $content;
+	return $content;
 }
 
 function getRelSyndicationFromSocial() {
@@ -32,7 +37,7 @@ function getRelSyndicationFromSocial() {
 	$services = $Social->instance()->services();
 	$broadcasts = array();
 	$see_on_social = "";
-	
+
 	if (is_array($ids) and count($ids)) {
 		foreach ($services as $key => $service) {
 			if (isset($ids[$key]) and count($ids[$key])) {
@@ -77,6 +82,92 @@ function getRelSyndicationFromSocial() {
 			}
 		}
 	}
+	return $see_on_social;
+}
+
+function getRelSyndicationFromSNAP() {
+	global $nxs_snapAvNts;
+	global $post;
+
+	$snap_options = get_option('NS_SNAutoPoster');
+	$urlmap = array (
+		'AP' => array(),
+		'BG' => array(),
+		'DA' => array(),
+		'DI' => array(),
+		'DL' => array(),
+		'FB' => array( 'url' => '%BASE%/posts/%pgID%' ),
+		'FF' => array(),
+		'FL' => array(),
+		'FP' => array(),
+		'GP' => array(),
+		'IP' => array(),
+		'LI' => array(),
+		'LJ' => array(),
+		'PK' => array(),
+		'PN' => array(),
+		'SC' => array(),
+		'ST' => array(),
+		'SU' => array(),
+		'TR' => array( 'url'=>'%BASE%/post/%pgID%' ),
+		'TW' => array( 'url'=>'%BASE%/status/%pgID%' ),
+		'VB' => array(),
+		'VK' => array(),
+		'WP' => array(),
+		'YT' => array(),
+	);
+
+	foreach ( $nxs_snapAvNts as $key => $serv ) {
+		/* all SNAP entries are in separate meta entries for the post based on the service name's "code" */
+		$mkey = 'snap'. $serv['code'];
+		$urlkey = $serv['lcode'].'URL';
+		$okey = $serv['lcode'];
+		$metas = maybe_unserialize(get_post_meta(get_the_ID(), $mkey, true ));
+		if ( !empty( $metas ) && is_array ( $metas ) ) {
+			foreach ( $metas as $cntr => $m ) {
+				$url = false;
+
+				if ( isset ( $m['isPosted'] ) && $m['isPosted'] == 1 ) {
+					/* this should be available for some services, for example Tumblr,
+					 * but buggy and misses slashes so URL ends up invalid
+					if ( isset( $m['postURL'] ) && !empty( $m['postURL'] ) ) {
+						$url = $m['postURL'];
+					}
+					else {
+					*/
+						$base = (isset( $urlmap[ $serv['code'] ]['url'])) ? $urlmap[ $serv['code'] ]['url'] : false;
+
+						if ( $base != false ) {
+							/* Facebook exception, why not */
+							if ( $serv['code'] == 'FB' ) {
+								$pos = strpos( $m['pgID'],'_' );
+								$pgID = ( $pos == false ) ? $m['pgID'] : substr( $m['pgID'], $pos + 1 );
+							}
+							else {
+								$pgID = $m['pgID'];
+							}
+
+							$o = $snap_options[ $okey ][$cntr];
+							$search = array('%BASE%', '%pgID%' );
+							$replace = array ( $o[ $urlkey ], $pgID );
+							$url = str_replace ( $search, $replace, $base );
+						}
+					/* } */
+
+					if ( $url != false ) {
+						$url = preg_replace('~(^|[^:])//+~', '\\1/', $url);
+						$classname = sanitize_title ( $serv['name'], $serv['lcode'] );
+						$broadcasts[] = '<li><a class="u-syndication link-'. $classname .' icon-'. $classname .'" rel="syndication" href="'. $url .'" target="_blank">'. $serv['name'] .'</a></li>';
+					}
+				}
+			}
+		}
+	}
+
+	if (count($broadcasts)) {
+		$see_on_social = '<ul>'.implode("\n", $broadcasts).'</ul>';
+	}
+
 	return $see_on_social;
 }
 ?>
